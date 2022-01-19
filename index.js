83348246860aa89adb99e1c33d4daddd9455dfc1
kartik <kartiksaini8881@gmail.com>
2022-01-19 13:00:40 +0530

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const mongoose=require('mongoose')
+const path=require('path')
 require('dotenv').config()
 const cors=require('cors')
 const userRoute=require('./routes/user')
